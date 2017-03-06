acc7a7164de3e86e4d528fe6ed72d68df633e00c
yakumo_17 <wo409697458@gmail.com>
2017-03-06 16:14:52 +0800

    json2

--- WebServer/app.py
+++ WebServer/app.py
@@ -1,16 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
 @app.route('/api/_get_config')
 def get_config():
-    t = {
-        'a': 1,
-        'b': 2,
-        'c': [3, 4, 5]
-    }
+    t = {'a': 1, 'b': 2, 'c': [3, 4, 5]}
     return jsonify(t)
+
+
+@app.route('/api/_post_status')
+def post_status():
+    data = request.get_json()
+    print(data)
 
 if __name__ == '__main__':
     app.debug = True
