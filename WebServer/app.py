dfaef46133896ea6bfd3018365ee883d72dbcd4b
yakumo_17 <wo409697458@gmail.com>
2017-03-06 18:31:07 +0800

    json5

--- WebServer/app.py
+++ WebServer/app.py
@@ -1,20 +1,33 @@
 from flask import Flask, jsonify, request,Response
 import json
 from flask_json import FlaskJSON, JsonError, json_response, as_json
+from urllib.request import urlopen
 
 app = Flask(__name__)
+json = FlaskJSON(app)
 
 
-@app.route('/config')
+@app.route('/config', methods=['get', 'post'])
 def get_config():
+    if request.method == 'post':
+        data = request.get_json(force=True)
+        print(type(data))
+        value = int(data['value'])
+        return json_response(plc=1, config=True, fuck_you=True)
     t = {'a': 1, 'b': 2, 'c': [3, 4, 5]}
-    return Response(json.dumps(t), mimetype='application/json')
-    # return jsonify(t)
+    #return Response(json.dumps(t), mimetype='application/json')
+    return jsonify(t)
 
 
-@app.route('/status', methods=['post'])
+@app.route('/status', methods=['get', 'post'])
 def post_status():
-    data = request.get_data()
+    data = request.get_json(force=True)
+    print(type(data))
+    value = int(data['value'])
+    return json_response(plc=1, config=True)
+    #data = json.loads(data)
+    #if data['id'] == 1:
+    #    return {'id':2}
 
 
 if __name__ == '__main__':
